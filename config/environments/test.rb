ce5085146bace30c1e23b24219db93a27376c0b9
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2015-09-11 22:02:53 -0700

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # Spped up testing
+  ActiveModel::SecurePassword.min_cost = true
 end
