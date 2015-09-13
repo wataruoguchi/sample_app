8044474423245d757b7f6ec1b78f434d1d0fbe56
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2015-09-12 22:50:00 -0700

    Finished Chapter 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
   end
 
   private
-  
+
     def create_remember_token
       self.remember_token = User.encrypt(User.new_remember_token)
     end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -40,22 +40,11 @@ describe "UserPages" do
 
     describe "with valid information" do
       before do
-        fill_in "Name", with: "Example User"
-        fill_in "Email", with: "user@example.com"
-        fill_in "Password", with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        valid_signup
       end
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
-      end
-
-      describe "after saving the user" do
-        before {click_button submit }
-        let(:user) { User.find_by(email: 'user@example.com') }
-
-        it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
 
       describe "after saving the user" do
@@ -64,7 +53,7 @@ describe "UserPages" do
 
         it { should have_link('Sign out') }
         it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_success_message('Welcome') }
       end
     end
   end
